536c333c9b39b06fcd8d4587c6da790983ee611a
YOUNES <YOUNES@DESKTOP-JH86H8A.mshome.net>
2018-10-20 11:52:01 +0100

    add remove service by zouani

--- src/service/CompteService.java
+++ src/service/CompteService.java
@@ -74,4 +74,16 @@ public class CompteService extends AbstractFacade<Compte> {
         }
     }
 
+    public int delete(String rib) {
+        Compte compte = find(rib);
+        if (compte == null) {
+            return -1;
+        } else if (compte.getSolde() != 0) {
+            return -2;
+        }else{
+            remove(compte);
+            return 1;
+        }
+    }
+
 }
